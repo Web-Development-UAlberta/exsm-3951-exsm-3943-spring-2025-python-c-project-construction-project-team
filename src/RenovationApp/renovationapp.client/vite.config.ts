815003a2859aff59641e85c573e5f273dd4ea224
E.Gamble <thegamblerkid@gmail.com>
2025-04-21 22:00:00 -0600

    Add docker compose, updatre server configs and targets.

--- src/RenovationApp/renovationapp.client/vite.config.ts
+++ src/RenovationApp/renovationapp.client/vite.config.ts
@@ -34,8 +34,10 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     }
 }
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7025';
+//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:8081';
+
+const target = 'https://localhost:63665/api'
 
 // https://vitejs.dev/config/
 export default defineConfig({
