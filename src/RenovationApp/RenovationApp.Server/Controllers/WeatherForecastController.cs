2bbb49a1a6798d70166447d151fc35debc8182a4
E.Gamble <thegamblerkid@gmail.com>
2025-04-22 21:32:11 -0600

    Working for global admin accounts;

--- src/RenovationApp/RenovationApp.Server/Controllers/WeatherForecastController.cs
+++ src/RenovationApp/RenovationApp.Server/Controllers/WeatherForecastController.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
 
 namespace RenovationApp.Server.Controllers
 {
     [ApiController]
     [Route("/api/[controller]")]
+    [Authorize]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -17,12 +20,40 @@ namespace RenovationApp.Server.Controllers
         {
             _logger = logger;
         }
-
+        [RequiredScope("user_impersonation")]
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+
+    public class RBACWeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public RBACWeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        [RequiredScope("user_impersonation")]
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<SecretWeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new SecretWeatherForecast
+            {
+                SecretLevel="Super duper secret",
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
--- src/RenovationApp/RenovationApp.Server/WeatherForecast.cs
+++ src/RenovationApp/RenovationApp.Server/WeatherForecast.cs
@@ -10,4 +10,17 @@ namespace RenovationApp.Server
 
         public string? Summary { get; set; }
     }
+
+    public class SecretWeatherForecast
+    {
+        public string? SecretLevel { get; set; }
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+
 }
