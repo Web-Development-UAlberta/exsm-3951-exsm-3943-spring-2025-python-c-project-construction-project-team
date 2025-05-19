35600bfc30d013ecc465debdf55be67ab3ce7ff4
Nina Shi <xiaojingshi.nina@gmail.com>
2025-05-19 12:12:25 -0600

    Update personal info tab layout

--- src/RenovationApp/renovationapp.client/src/pages/client/account/PersonalInfoTab.tsx
+++ src/RenovationApp/renovationapp.client/src/pages/client/account/PersonalInfoTab.tsx
@@ -10,65 +10,42 @@ const PersonalInfoTab: React.FC<PersonalInfoTabProps> = ({ graphData }) => {
         window.location.href = "https://myaccount.microsoft.com/";
     }
 
-
     return (
-        <>
-            <div className="mb-5">
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                    <h3 className="border-bottom w-100 pb-2">Personal Information</h3>
-                    <button
-                        className="btn btn-sm btn-outline-secondary ms-3"
-                        onClick={onClick}
-                    >
-                        Edit
-                    </button>
-                </div>
-                <div className="row">
-                    <div className="col-md-6 mb-3">
-                        <label className="fw-bold d-block">First Name</label>
-                        <span>{graphData?.givenName}</span>
-                    </div>
-                    <div className="col-md-6 mb-3">
-                        <label className="fw-bold d-block">Last Name</label>
-                        <span>{graphData?.surname}</span>
-                    </div>
-                    <div className="col-md-6 mb-3">
-                        <label className="fw-bold d-block">Email Address</label>
-                        <span>{graphData?.mail}</span>
-                    </div>
-                    <div className="col-md-6 mb-3">
-                        <label className="fw-bold d-block">Phone Number</label>
-                        <span>{graphData?.mobilePhone}</span>
-                    </div>
-                </div>
-
+        <div className="mb-5">
+            <div className="d-flex justify-content-between align-items-center mb-3">
+                <h3>Personal Information</h3>
+                <button
+                    className="btn btn-sm btn-outline-secondary ms-3"
+                    onClick={onClick}
+                >
+                    Edit
+                </button>
             </div>
-
-            <div className="mb-4">
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                    <h3 className="border-bottom w-100 pb-2">Address</h3>
-                    <button
-                        className="btn btn-sm btn-outline-secondary ms-3"
-                        onClick={onClick}
-                    >
-                        Edit
-                    </button>
+            <div className="row">
+                <div className="col-md-6 mb-3">
+                    <label className="fw-bold d-block">First Name</label>
+                    <span>{graphData?.givenName}</span>
                 </div>
-
-                {/* Display address */}
-
-                <div className="p-3 mb-3">
-                    <div className="d-flex justify-content-between">
-                        <div>
-                            <div>{graphData?.streetAddress}</div>
-                            <div>{graphData?.city}, {graphData?.state} {graphData?.postalCode}</div>
-                            <div>{graphData?.country}</div>
-                        </div>
-                    </div>
+                <div className="col-md-6 mb-3">
+                    <label className="fw-bold d-block">Last Name</label>
+                    <span>{graphData?.surname}</span>
+                </div>
+                <div className="col-md-6 mb-3">
+                    <label className="fw-bold d-block">Email Address</label>
+                    <span>{graphData?.mail}</span>
+                </div>
+                <div className="col-md-6 mb-3">
+                    <label className="fw-bold d-block">Phone Number</label>
+                    <span>{graphData?.mobilePhone ?? "N/A"}</span>
+                </div>
+                <div className="col-md-6 mb-3">
+                    <label className="fw-bold d-block">Address</label>
+                    <span>{graphData?.streetAddress}</span>
+                    <span>{graphData?.city}, {graphData?.state} {graphData?.postalCode} </span>
+                    <span>{graphData?.country}</span>
                 </div>
             </div>
-
-        </>
+        </div>
     )
 }
 
